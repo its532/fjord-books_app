dc0a0c13f347dbb4389acefba9d3fcd7bc3fe175
itsuki.kikuyama <tf.k.itsuki.rits@gmail.com>
2021-11-11 18:22:43 +0900

    userモデルのテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,41 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @alice = users(:alice)
+    @bob = users(:bob)
+  end
+
+  test '#name_or_email When you have a name' do
+    assert_equal 'alice', @alice.name_or_email
+  end
+
+  test '#name_or_email When you don not have a name' do
+    assert_equal 'bob@example.com', @bob.name_or_email
+  end
+
+  test '#follow　When Alice followed Bob' do
+    assert_not @alice.following?(@bob)
+    @alice.follow(@bob)
+    assert @alice.following?(@bob)
+  end
+
+  test '#following?　When Alice followed Bob' do
+    assert_not @alice.following?(@bob)
+    @alice.follow(@bob)
+    assert @alice.following?(@bob)
+  end
+
+  test '#followed_by?　When Alice followed Bob' do
+    assert_not @alice.followed_by?(@bob)
+    @alice.follow(@bob)
+    assert @bob.followed_by?(@alice)
+  end
+
+  test '#unfollow　When Alice unfollowed Bob' do
+    @alice.follow(@bob)
+    assert @alice.following?(@bob)
+    @alice.unfollow(@bob)
+    assert_not @alice.following?(@bob)
+  end
 end
