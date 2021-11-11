df05869dd2ad05fe0613486edc758b761af40c76
itsuki.kikuyama <tf.k.itsuki.rits@gmail.com>
2021-11-11 18:23:23 +0900

    reportモデルのテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,17 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @report = reports(:repo)
+    @alice = users(:alice)
+    @bob = users(:bob)
+  end
+
+  test '#editable?' do
+    assert @report.editable?(@alice)
+  end
+
+  test '#created_on' do
+    assert_equal(@report.created_at.to_date, @report.created_on)
+  end
 end
