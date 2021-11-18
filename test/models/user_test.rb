f52c0aece156e77665f3a3474bd191f8c2cf475d
itsuki.kikuyama <tf.k.itsuki.rits@gmail.com>
2021-11-18 12:17:24 +0900

    テストの日本語化と１テストに複数モデルのメソッドが使われていた問題に対応

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,38 +6,41 @@ class UserTest < ActiveSupport::TestCase
   def setup
     @alice = users(:alice)
     @bob = users(:bob)
+    @marry = users(:marry)
+    @jin = users(:jin)
   end
 
-  test '#name_or_email When you have a name' do
+  test '#name_or_email nameの値を持っている時' do
     assert_equal 'alice', @alice.name_or_email
   end
 
-  test '#name_or_email When you don not have a name' do
+  test '#name_or_email nameの値を持っていない時' do
     assert_equal 'bob@example.com', @bob.name_or_email
   end
 
-  test '#follow　When Alice followed Bob' do
-    assert_not @alice.following?(@bob)
+  test '#follow　AliceがBobをフォローした時' do
     @alice.follow(@bob)
-    assert @alice.following?(@bob)
+    assert @alice.active_relationships.present?
   end
 
-  test '#following?　When Alice followed Bob' do
+  test '#following?　AliceはBobをフォローしていない' do
     assert_not @alice.following?(@bob)
-    @alice.follow(@bob)
-    assert @alice.following?(@bob)
   end
 
-  test '#followed_by?　When Alice followed Bob' do
+  test '#following?　MarryはJinをフォローしている' do
+    assert @marry.following?(@jin)
+  end
+
+  test '#followed_by?　AliceのフォロワーにBobはいない' do
     assert_not @alice.followed_by?(@bob)
-    @alice.follow(@bob)
-    assert @bob.followed_by?(@alice)
   end
 
-  test '#unfollow　When Alice unfollowed Bob' do
-    @alice.follow(@bob)
-    assert @alice.following?(@bob)
-    @alice.unfollow(@bob)
-    assert_not @alice.following?(@bob)
+  test '#followed_by?　JinのフォロワーにMarryがいる' do
+    assert @jin.followed_by?(@marry)
+  end
+
+  test '#unfollow　MarryがJinのフォローを解除した時' do
+    @marry.unfollow(@jin)
+    assert_not @marry.active_relationships.present?
   end
 end
